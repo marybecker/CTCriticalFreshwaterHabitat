17f76897a9c4f03f36b90e7f44b9f290ec6de552
marybecker <mary.becker@ct.gov>
2019-06-16 22:02:03 -0400

    add draft WBK map

--- wbk.sql
+++ wbk.sql
@@ -1,25 +1,4 @@
-drop table if exists
-	wbkprj.ctcatch_ici_iwi;
-
-create table
-	wbkprj.ctcatch_ici_iwi
-as
-select
-	wbkprj.nhdcatchment.featureid,
-	wbkprj.nhdcatchment.geom,
-	wbkprj.nhdflowline.gnis_name,
-	wbkprj.ici_iwi.catareasqkm,
-	wbkprj.ici_iwi.wsareasqkm,
-	wbkprj.ici_iwi.ici::numeric,
-	wbkprj.ici_iwi.iwi::numeric,
-	(wbkprj.ici_iwi.ici::numeric+wbkprj.ici_iwi.iwi::numeric)/2 as wqindex
-from
-	((wbkprj.nhdcatchment
-inner join
-	wbkprj.ici_iwi on wbkprj.nhdcatchment.featureid = wbkprj.ici_iwi.comid::numeric)
-inner join
-	wbkprj.nhdflowline on wbkprj.nhdcatchment.featureid = wbkprj.nhdflowline.comid);
-
+/*Identify stream segments with high density wbk and calculate CT specific IWI index */
 drop table if exists
 	wbkprj.wbk;
 
@@ -27,27 +6,67 @@ create table
 	wbkprj.wbk
 as
 select
-	wbkprj.ctstreamflow.hydroid,
+	wbkprj.ctstreamflow.id,
 	wbkprj.ctstreamflow.geom,
-	wbkprj.ctstreamflow.strahler
+	wbkprj.ctstreamflow.hydroid,
+	wbkprj.ctstreamflow.icmetric,
+	wbkprj.ctstreamflow.divmetric,
+	wbkprj.ctstreamflow.dammetric,
+	wbkprj.ctstreamflow.stpmetric,
+	((wbkprj.ctstreamflow.icmetric+wbkprj.ctstreamflow.divmetric+wbkprj.ctstreamflow.dammetric+wbkprj.ctstreamflow.stpmetric)-4)/12as iwi
 from
 	wbkprj.ctstreamflow
 where
 	wbkprj.ctstreamflow.wbk = 'Yes';
 
+drop table if exists
+	wbkprj.wbkcatchment;
+
+/* Join wbk streams to stream catchment areas */
+create table
+	wbkprj.wbkcatchment
+as
 select
-	wbkprj.ctcatch_ici_iwi.featureid,
-	wbkprj.ctcatch_ici_iwi.geom,
-	wbkprj.ctcatch_ici_iwi.gnis_name,
-	wbkprj.ctcatch_ici_iwi.catareasqkm,
-	wbkprj.ctcatch_ici_iwi.wsareasqkm,
-	wbkprj.ctcatch_ici_iwi.ici,
-	wbkprj.ctcatch_ici_iwi.iwi,
-	wbkprj.ctcatch_ici_iwi.wqindex,
-	wbkprj.wbk.hydroid
+	wbkprj.wbk.hydroid,
+	wbkprj.wbk.iwi,
+	wbkprj.hsidata.geom,
+	wbkprj.hsidata.strmi,
+	wbkprj.hsidata.sqmi,
+	wbkprj.hsidata.strahler
 from
-	wbkprj.ctcatch_ici_iwi
+	wbkprj.hsidata
 join
 	wbkprj.wbk
 on
-	st_contains(wbkprj.ctcatch_ici_iwi.geom,st_centroid(wbkprj.wbk.geom));
+	wbkprj.hsidata.drainid = wbkprj.wbk.hydroid;
+
+/* spatial join to get iwi at sample point */
+
+create table
+	wbkprj.fishsites_iwi
+as
+select
+	wbkprj.fishsites.sta_seq,
+	wbkprj.fishsites.geom,
+	wbkprj.fishsites.station_na as name,
+	wbkprj.wbkcatchment.iwi
+from
+	wbkprj.fishsites
+join
+	wbkprj.wbkcatchment
+on
+	st_intersects(wbkprj.fishsites.geom,wbkprj.wbkcatchment.geom);
+
+/* Sum WBK Stream Length */
+
+select
+	sum(st_length(wbkprj.wbk.geom))/5280
+from
+	wbkprj.wbk;
+
+select
+	sum(st_length(wbkprj.wbk.geom))/5280
+from
+	wbkprj.wbk
+where
+	wbkprj.wbk.iwi > 0;
